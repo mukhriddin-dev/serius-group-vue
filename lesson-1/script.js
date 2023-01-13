6d4cb0b922d014f7f267a184e8a9f1061bf4511e
mukhriddin-dev <khodievm@gmail.com>
2023-01-13 21:47:52 +0500

    ok

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -1,1 +1,70 @@
-const app=1;
+//console.log(a); // undefinde
+//console.log(app); // is not defined
+
+let ok = null; // null
+
+// let number=12223455546843683488538458348582385823n;
+
+let id = Symbol(1);
+
+// console.log(id)
+// console.log(number);
+
+const sym2 = Symbol("foo");
+const sym3 = Symbol("foo");
+
+// console.log(sym2);
+// console.log(sym3)
+// console.log(sym2==sym3)
+
+let a = 12;
+let b = 23;
+
+// a=a+b;
+// a=a-b;
+// a=a*b;
+
+// a+=b;
+// a-=b;
+// a*=b;
+
+// console.log(a);
+
+// ==
+// ===
+// !=
+// !==
+// !
+
+// Math.abs() =>
+
+// Math.ceil() =>
+
+// Math.floor() =>
+
+// Math.round() =>
+
+// Math.random() =>
+
+// Math.pow(2, 5) =>
+
+// Math.sqrt(49) =>
+
+// Math.max(1,2,3,4,5)
+
+// Math.min(1,3,4)
+
+let num = "12";
+let num2 = Number(num); // 2-usuli
+
+let text = 12;
+let text2 = String(text);
+
+console.log(text2);
+console.log(typeof text.toString());
+
+
+
+
+// 1. node.js 
+// 2. 
---
+++ lesson-2/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <header class="bg-success">
+    <div class="container">
+      <nav class="nav bg-success text-white d-flex justify-content-between">
+        <h3 class="nav__logo">VUE.JS</h3>
+        <ul class="nav__list d-flex justify-content-between w-25 list-unstyled">
+          <li class="nav__list--item">
+            <a href="#" class="nav__ul--list-link">Home</a>
+          </li>
+          <li class="nav__list--item">
+            <a href="#" class="nav__ul--list-link">About</a>
+          </li>
+          <li class="nav__list--item">
+            <a href="#" class="nav__ul--list-link">Contact</a>
+          </li>
+          <li class="nav__list--item">
+            <a href="#" class="nav__ul--list-link">Service</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <!-- header end -->
+
+  <main>
+    <div class="container">
+      <!-- main content -->
+
+      <div class="card w-75 mx-auto p-5 shadow mt-5">
+        <h1 class="text-center">{{ num }}</h1>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+          <button type="button" class="btn btn-danger" v-on:click="dicrement">
+            DICREMENT
+          </button>
+          <button type="button" class="btn btn-success" v-on:click="increment">
+            INCREMENT
+          </button>
+          <button type="button" class="btn btn-warning" v-on:click="resetCount">
+            RESET
+          </button>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <footer class="p-3 text-center text-white bg-success">
+    <div class="container">
+      <p>{{ new Date().getFullYear() }} Vue.js</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 0,
+      str: "Vue.js",
+    };
+  },
+
+  methods: {
+    dicrement() {
+      console.log((this.num -= 1));
+    },
+    increment() {
+      console.log((this.num += 1));
+    },
+    resetCount() {
+      this.num = 0;
+    },
+  },
+};
+</script>
+
+<style>
+main {
+  height: calc(100vh - 70px - 65px);
+}
+.nav {
+  height: 75px;
+  align-items: center;
+  display: flex;
+}
+
+.nav__ul--list-link {
+  text-decoration: none;
+  color: white;
+}
+</style>
